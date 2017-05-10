451c85bc8a8bbe9fbac0d2a220cc075fe8163797
Rob Schoenbeck <robs@kiva.org>
2017-05-10 11:15:38 -0700

    Remove -b option from psql

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "Deploying tables..."
 find sql -name \*.sql | while read sql_file; do
 	echo "Deploying file '$sql_file'"
 	table_name=`echo $sql_file | grep -E "[A-Za-z_]+.sql" -o | sed 's/.\{4\}$//'`
-	psql -b -v tableschema="$db_schema" -v tablename="$table_name" -h $db_host -d $db_name -U $db_user -f $sql_file
+	psql -v tableschema="$db_schema" -v tablename="$table_name" -h $db_host -d $db_name -U $db_user -f $sql_file
 	echo "Setting grants for table '$table_name'"
-	psql -b -v tableschema="$db_schema" -v tablename="$table_name" -h $db_host -d $db_name -U $db_user -f grants/grants.sql
+	psql -v tableschema="$db_schema" -v tablename="$table_name" -h $db_host -d $db_name -U $db_user -f grants/grants.sql
 done
